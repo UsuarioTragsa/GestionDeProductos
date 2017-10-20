e8c7f05154d6a3752e73afe349c5236dc62d593d
UsuarioTragsa <alberto.lendinezperez@gmail.com>
2017-10-20 13:42:06 +0200

    Creando interfaces

--- src/vista/DetallesProducto.java
+++ src/vista/DetallesProducto.java
@@ -3,10 +3,19 @@ package vista;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class DetallesProducto {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -36,8 +45,44 @@ public class DetallesProducto {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 584, 472);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Usuario:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel.setBounds(315, 27, 99, 21);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
+		lblContrasea.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblContrasea.setBounds(315, 59, 84, 21);
+		frame.getContentPane().add(lblContrasea);
+		
+		textField = new JTextField();
+		textField.setBounds(402, 29, 145, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(402, 59, 145, 20);
+		frame.getContentPane().add(passwordField);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setBounds(315, 104, 99, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Registrarse");
+		btnNewButton_1.setBounds(448, 104, 99, 23);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(27, 182, 520, 229);
+		frame.getContentPane().add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(27, 11, 199, 160);
+		frame.getContentPane().add(panel_1);
 	}
-
 }
--- src/vista/Registro.java
+++ src/vista/Registro.java
@@ -3,6 +3,12 @@ package vista;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
 
 public class Registro {
 
@@ -36,8 +42,63 @@ public class Registro {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 541, 515);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JLabel lblNewLabel = new JLabel("Nombre:");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 2;
+		frame.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblDni = new JLabel("DNI:");
+		lblDni.setHorizontalAlignment(SwingConstants.LEFT);
+		GridBagConstraints gbc_lblDni = new GridBagConstraints();
+		gbc_lblDni.insets = new Insets(0, 0, 5, 0);
+		gbc_lblDni.gridx = 1;
+		gbc_lblDni.gridy = 3;
+		frame.getContentPane().add(lblDni, gbc_lblDni);
+		
+		JLabel lblDomicilio = new JLabel("Domicilio:");
+		GridBagConstraints gbc_lblDomicilio = new GridBagConstraints();
+		gbc_lblDomicilio.insets = new Insets(0, 0, 5, 0);
+		gbc_lblDomicilio.gridx = 1;
+		gbc_lblDomicilio.gridy = 4;
+		frame.getContentPane().add(lblDomicilio, gbc_lblDomicilio);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
+		gbc_lblEmail.insets = new Insets(0, 0, 5, 0);
+		gbc_lblEmail.gridx = 1;
+		gbc_lblEmail.gridy = 5;
+		frame.getContentPane().add(lblEmail, gbc_lblEmail);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
+		gbc_lblPassword.insets = new Insets(0, 0, 5, 0);
+		gbc_lblPassword.gridx = 1;
+		gbc_lblPassword.gridy = 6;
+		frame.getContentPane().add(lblPassword, gbc_lblPassword);
+		
+		JLabel lblRepetirPassword = new JLabel("Repetir Password:");
+		GridBagConstraints gbc_lblRepetirPassword = new GridBagConstraints();
+		gbc_lblRepetirPassword.insets = new Insets(0, 0, 5, 0);
+		gbc_lblRepetirPassword.gridx = 1;
+		gbc_lblRepetirPassword.gridy = 7;
+		frame.getContentPane().add(lblRepetirPassword, gbc_lblRepetirPassword);
+		
+		JLabel lblFechaDeNacimiento = new JLabel("Fecha de nacimiento:");
+		GridBagConstraints gbc_lblFechaDeNacimiento = new GridBagConstraints();
+		gbc_lblFechaDeNacimiento.gridx = 1;
+		gbc_lblFechaDeNacimiento.gridy = 8;
+		frame.getContentPane().add(lblFechaDeNacimiento, gbc_lblFechaDeNacimiento);
 	}
 
 }
